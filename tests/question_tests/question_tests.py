f0d0c2a406c677becc68571592947dadfc5c9284
LaQuira Logan <laquira.logan@g.austincc.edu>
2025-10-15 01:21:57 +0000

    Is number prime checker.

--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,20 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_b.question_b import check_if_prime, is_prime
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
 
+    def test_check_prime(self):
+        from src.question_b.question_b import is_prime
+        self.assertEqual(is_prime(4), False)
+        self.assertEqual(is_prime(5), True)
+        self.assertEqual(is_prime(11), True)
 
+def test_config():
+    return True
+        
+        
