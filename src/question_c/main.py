952817dfbca957e6adce522686a496d69d3e2141
LaQuira Logan <laquira.logan@g.austincc.edu>
2025-10-16 18:10:42 +0000

    Convert a Number to a Week Day

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,8 @@
-#add import
+from question_c import get_day_of_week, convert_day_number
+
+def main():
+    convert_day_number()
+
+if __name__ == "__main__":
+    main()
+    
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,19 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_c.question_c import get_day_of_week, convert_day_number
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
 
+    def test_get_day_of_week(self):
+        self.assertEqual(get_day_of_week(0), "Invalid, please enter a number between 1 and 7.")
+        self.assertEqual(get_day_of_week(1), "Monday")
+        self.assertEqual(get_day_of_week(2), "Tuesday")
+        self.assertEqual(get_day_of_week(3), "Wednesday")
+        self.assertEqual(get_day_of_week(8), "Invalid, please enter a number between 1 and 7.")
 
+def test_config():
+    return True
