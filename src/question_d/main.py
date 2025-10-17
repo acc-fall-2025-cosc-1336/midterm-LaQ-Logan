104ede6db4c00149908043a54dcc9cb5c7f83078
LaQuira Logan <laquira.logan@g.austincc.edu>
2025-10-17 00:25:13 +0000

    Calculating bonus pay off sales amount

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,8 @@
-#add import
+from question_d import get_bonus_pay_amount, give_bonus_pay
+
+def main():
+    give_bonus_pay()    
+
+
+if __name__ == "__main__":
+    main()
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,22 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_d.question_d import get_bonus_pay_amount, give_bonus_pay
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
+
+    def test_get_bonus_pay_amount(self):
+        self.assertEqual(get_bonus_pay_amount(-1), (0.0, 0))
+        self.assertEqual(get_bonus_pay_amount(200), (10.0, 0.05))
+        self.assertEqual(get_bonus_pay_amount(600), (36.0, 0.06))
+        self.assertEqual(get_bonus_pay_amount(1000), (70.0, 0.07))
+        self.assertEqual(get_bonus_pay_amount(1500), (120.0, 0.08))
+        self.assertEqual(get_bonus_pay_amount(2000), (0.0, 0))
+
+def test_config():
+    return True
 
 
